9f51016e4dd0f74df6db2cbe022d6be381d57d16
Aver30 <aver30@student.monash.edu>
2018-10-21 20:28:22 +1100

    Created New Template
    Created new template

---
+++ themes/Ass3_TeamBlue/footer.php
@@ -0,0 +1,53 @@
+<div class="jumbotron text-center" style="margin-bottom:0px">
+    <div class="container no-gutter">
+        <div class="row no-gutter" style="margin-top: auto; padding-top: 0px">
+            <div class="col-sm-6">
+
+                <img src="wp-content/themes/Ass3_TeamBlue/Sponsor1.png" width=100px height=60px>
+
+                <img src="wp-content/themes/Ass3_TeamBlue/Sponsor2.png" width=100px height=60px>
+
+                <img src="wp-content/themes/Ass3_TeamBlue/Sponsor3.png" width=100px height=60px>
+
+                <img src="wp-content/themes/Ass3_TeamBlue/Sponsor4.png" width=100px height=60px>
+
+            </div>
+            <div class="col-sm-5">
+                <form action="/action_page.php">
+                 <div class="form-group" style="text-align: left">
+                 <label for="email">Suscribe Now!</label>
+                  <div class="form-inline">
+                    <input type="email" class="form-control" id="email" size="30" placeholder="Enter Email Address">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                  </div>
+                 </div>
+                </form>
+            </div>
+
+
+
+        </div>
+        <div width="100%">
+            <h2>Follow Us On:</h2>
+            <img src="wp-content/themes/Ass3_TeamBlue/social-media-icons.png" height="70px" width="90px">
+            </div>
+
+
+
+    </div>
+    <footer class="footer" style="padding-top: 30px; background-color: #E8E8E8">
+      <div class="container" style="text-align: center">
+        <span class="text-muted">
+        HRAFF - Human Rights Arts & Film Festival - hraff.org.au  -
+            <a href="#">Contant Us</a>  - <a href="#"> Terms & Conditions</a> - <a href="#"> Privacy</a>
+
+        </span>
+      </div>
+        <?php wp_footer(); ?> <!-- Include this before clsoing body -->
+    </footer>
+
+</div>
+
+
+</body>
+</html>
---
+++ themes/Ass3_TeamBlue/header.php
@@ -0,0 +1,49 @@
+<html>
+<head>
+	<title><?php wp_title( '-', true, 'right' ); bloginfo( 'name' ); ?></title>
+  <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
+  <?php wp_head(); ?>	<!-- Put this before closing head-->
+</head>
+
+<body>
+	<div class="row no-gutters" style="background-color: white">
+			<div class="col-sm-5">
+			<div style="margin-bottom:0">
+				<img src="wp-content/themes/Ass3_TeamBlue/LogoBackground2.png" style="height: 140px; width:100%">
+				</div>
+			</div>
+	</div>
+	<!-- This gets the header from the WP. But we want the Logo as the header.
+  <header>
+    <h1><a href="<?php bloginfo( 'url' ); ?>"><?php bloginfo( 'name' ) ?></a></h1>
+  </header>
+	-->
+	<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+	  <a class="navbar-brand" href="#">Navbar</a>
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+	    <span class="navbar-toggler-icon"></span>
+	  </button>
+	  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+	    <ul class="navbar-nav">
+	      <li class="nav-item">
+	        <a class="nav-link" href="#">Link 1</a>
+	      </li>
+	      <li class="nav-item">
+	        <a class="nav-link" href="#">Link 2</a>
+	      </li>
+	      <li class="nav-item">
+	        <a class="nav-link" href="#">Link 3</a>
+	      </li>
+	    </ul>
+	  </div>
+	</nav>
+<!-- Not ssure how this is needed leaving it here just incase we need it later
+  <nav>
+    <?php wp_nav_menu( [ 'theme_location' => 'main-menu' ] ); ?>
+  </nav>
+-->
