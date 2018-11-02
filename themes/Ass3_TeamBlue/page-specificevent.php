69953355ed5d3f9f2fad5f9e3dd6696183dcdc3d
Deexita Goli <deexi333@users.noreply.github.com>
2018-11-02 16:04:11 +1100

    hello

--- themes/Ass3_TeamBlue/page-specificevent.php
+++ themes/Ass3_TeamBlue/page-specificevent.php
@@ -4,8 +4,6 @@
 ?>
 
   <?php
-
-      echo '<div class="fill">';
       // This Provides the Event Post ID.
       $var_value = $_GET["?name"];
       // $event_post = get_post($var_value); // not the best way
@@ -23,30 +21,67 @@
       $event_review = get_field('event_review');
 
 
+      echo '<h1 style="font-weight: bold;text-align: center; margin-left: 2vw; margin-right: 2vw;">';
+        print_r($event_name);
+      echo '</h1>';
+
+
+      echo '<div class="fill">';
       // This Prints Image.
       echo '<img src="';
       print_r($event_image);
-      echo '" style="width: 10rem">';
+      echo '">';
 
-     // End row
-     echo '</div>';
+      echo '</div>';
 
-     // event quote
-     echo '<div>';
-       echo '<h4>"';
-       print_r($event_review);
-       echo '"</h4>';
-     echo'</div>';
+      echo '<div style="font-weight: bold;text-align: center; margin-left: 2vw; margin-right: 2vw;">"';
+      print_r($event_review);
+      echo '"</div>';
+      echo '<div style="font-weight: bold;text-align: center;">-';
+      print_r($event_reviewer);
+      echo '</div>';
+      echo '<br>';
 
-     echo '<div>';
-     print_r($event_des);
+      echo'<div class="row">';
+
+       // event quote
+       echo '<div class="col-sm-6">';
+           echo '<div style="text-align: center; margin-left: 2vw; margin-right: 2vw;">';
+           print_r($event_des);
+           echo '</div>';
+
+       echo'</div>';
+
+       echo '<div class="col-sm-6">';
+        echo '<div style="font-weight: bold; text-align: center; margin-left: 2vw; margin-right: 2vw;">When</div>';
+
+         echo '<div style="text-align: center; margin-left: 2vw; margin-right: 2vw;">';
+         $format_in = 'F j, Y'; // the format your value is saved in (set in the field options)
+         $format_out = 'F j, Y'; // the format you want to end up with
+         $date = DateTime::createFromFormat($format_in, get_field('event_date'));
+        print_r($date->format( $format_out));
+         echo '</div>';
+
+         echo '<br>';
+
+         echo '<div style="font-weight: bold; text-align: center; margin-left: 2vw; margin-right: 2vw;">Where</div>';
+
+         echo '<div style="text-align: center; margin-left: 2vw; margin-right: 2vw;">';
+          print_r($event_location);
+         echo '</div>';
+
+
+       echo'</div>';
+
+
+     // end row
      echo '</div>';
 
   ?>
 
-<button>Book Tickets</button>
 
-</div>
+
+  </div>
 
 <?php
   get_footer();
